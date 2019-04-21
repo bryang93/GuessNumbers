eba291e04818fa1fdade0abe5c6720d43d314a33
bryang93 <47666895+bryang93@users.noreply.github.com>
2019-04-20 20:06:26 -0400

    Add project files.

---
+++ GuessNumber/BiSection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumber
+{
+    class BiSection
+    {
+        public List<int> Numbers { get; set; }
+
+        public void NumbersAvailable(List<int> Numbers)
+        {
+            Console.WriteLine("Numbers Available");
+            foreach (var i in this.Numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ GuessNumber/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int input = int.Parse(Console.ReadLine());
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            bool findLoop = false;
+            while (findLoop == false)
+            {
+                if (input == numbers[numbers.Count / 2])
+                {
+                    Console.WriteLine($"You're number is: {numbers[numbers.Count / 2]}");
+                    findLoop = true;
+                }
+                else if (input < numbers[numbers.Count / 2])
+                {
+                    numbers.Reverse();
+                    numbers.RemoveRange(0, numbers.Count / 2);
+                    Console.WriteLine("Numbers Available");
+                    foreach (var i in numbers)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    numbers.Reverse();
+                }
+                else if (input > numbers[numbers.Count / 2])
+                {
+                    numbers.RemoveRange(0, numbers.Count / 2);
+                    Console.WriteLine("Numbers Available");
+                    foreach (var i in numbers)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+        }
+    }
+}
